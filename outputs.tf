96efe4287ae66cbd4059090639378b1173dfc64b
David Negstad <davidn@pk4media.com>
2016-01-11 01:49:10 -0800

    Security group is passed in now

--- outputs.tf
+++ outputs.tf
@@ -8,10 +8,6 @@ output "private_ips" {
   value = "${join(",", aws_instance.vault.*.private_ip)}"
 }
 
-output "client_security_group_id" {
-  value = "${aws_security_group.client.id}"
-}
-
 output "consul_datacenter" {
   value = "${var.region}"
 }
