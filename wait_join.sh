5f1fcee5be5c48cb5875c29d697c05c83f2afb64
David Negstad <davidn@pk4media.com>
2016-01-11 16:26:24 -0800

    Update how wait join works for consul clients

--- wait_join.sh
+++ wait_join.sh
@@ -8,10 +8,10 @@ LOCAL_IPV4=`curl http://169.254.169.254/2014-02-25/meta-data/local-ipv4`
 # Vault setup and other places, set a ready value in its KV store
 # to signify that it is ready.
 SLEEPTIME=1
-cget_peers() { curl -sf "http://127.0.0.1:8500/v1/status/peers"; }
+cget_leaders() { curl -sf "http://127.0.0.1:8500/v1/status/leader"; }
 
 # Wait for the Consul cluster to become ready
-while ! cget_peers | grep $LOCAL_IPV4:8300; do
+while cget_leader | grep "^\"\"$"; do
   if [ $SLEEPTIME -ge 24 ]; then
     echo "ERROR: CONSUL DID NOT COMPLETE SETUP! Manual intervention required on node $METADATA_INSTANCE_ID)}."
     exit 2
